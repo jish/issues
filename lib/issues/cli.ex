b85b94ac1ea92aefc498c1aa3922480877073843
Josh Lubaway <josh.lubaway@gmail.com>
2018-02-20 20:42:36 -0800

    Parse arguments

---
+++ lib/issues/cli.ex
@@ -0,0 +1,44 @@
+defmodule Issues.Cli do
+
+  @default_count 4
+
+  @moduledoc """
+  Handle the command line parsing and
+  dispatch to various functions that will eventually
+  generate a table of the last _n_ issues of a GitHub project.
+  """
+
+  def run(argv) do
+    parse_args(argv)
+  end
+
+  @doc """
+  `argv` can be `-h` or `--help`, which returns `:help`
+
+  Otherwise, return a GitHub username, projectname and
+  (optionally) the number of entries to format.
+
+  Return a tuple of `{ user, project, count }`, or
+  the atom `:help` if help was requested.
+  """
+
+  def parse_args(argv) do
+    parse = OptionParser.parse(argv,
+      switches: [help: :boolean],
+      aliases: [h: :help])
+
+    case parse do
+      { [ help: true ], _, _ }
+        -> :help
+
+      { _, [user, project, count], _ }
+        -> { user, project, String.to_integer(count) }
+
+      { _, [user, project], _ }
+        -> { user, project, @default_count }
+
+      _ -> :help
+    end
+  end
+
+end
