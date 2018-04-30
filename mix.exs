03f196d291b20d80f2d96b73ddcc7d2cbbb321a8
Josh Lubaway <josh.lubaway@gmail.com>
2018-04-29 18:22:39 -0700

    Add documentation

--- mix.exs
+++ mix.exs
@@ -7,6 +7,7 @@ defmodule Issues.Mixfile do
       version: "0.1.0",
       elixir: "~> 1.5",
       escript: escript_config(),
+      source_url: "https://github.com/jish/issues",
       start_permanent: Mix.env == :prod,
       deps: deps()
     ]
@@ -24,8 +25,10 @@ defmodule Issues.Mixfile do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
-      httpoison: "~> 1.0",
-      poison: "~> 3.1"
+      {:earmark, "~> 1.2.5"},
+      {:ex_doc, "~> 0.16", only: :dev, runtime: false},
+      {:httpoison, "~> 1.0"},
+      {:poison, "~> 3.1"}
     ]
   end
 
